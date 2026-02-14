a0bb35556b86a224e274d6e141a5eff4a1a43bd3
Toppuwittaya <toppuwittaya@gmail.com>
2026-02-14 22:18:20 +0700

    fix type to calc budget

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post("/users", async (req, res) => {
         const result = await pool.query("INSERT INTO users(name,email) VALUES($1,$2) RETURNING *", [name, email]);
         res.json(result.rows[0]);
     } catch (err) {
-        console.error(err);
+        console.error(err); 
         res.status(500).json({ error: "DB Error" });
     }
 });
